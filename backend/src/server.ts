cbebbf25a8cbddadfd2c39816073e6e1ca3e28ab
okancingoz <okanc.ngoz@gmail.com>
2025-07-23 13:49:51 +0300

    feat: setup init express server with mongodb connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,21 @@
+import dotenv from "dotenv";
 import express from "express";
+import { connectDB } from "./config/db";
+
+dotenv.config();
 
 const app = express();
-
-app.get("/", (_req, res) => {
-  res.send("FluAI Backend is running!");
-});
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+(async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error starting server:", err);
+    process.exit(1); // Exit the process with failure
+  }
+})();
